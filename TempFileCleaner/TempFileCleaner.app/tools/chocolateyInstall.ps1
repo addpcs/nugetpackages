150d2473302c25718a78bbd79f1ea832764ee801
Justin Murray <murrayju@addpcs.com>
2013-05-29 20:40:54 -0400

    Added TempFileCleaner Chocolatey package.

---
+++ TempFileCleaner/TempFileCleaner.app/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'TempFileCleaner.app'
+$installerType = 'EXE'
+$url = 'http://software.addpcs.com/get.php?dl=50'
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
---
+++ TempFileCleaner/TempFileCleaner/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿#Install-VirtualPackage 'TempFileCleaner.tool' 'TempFileCleaner.app'
